b129630144cc0686bcf1939a9e50ba9247e84a89
McWooden <garryf7887@gmail.com>
2023-11-05 00:10:44 +0700

    berhasil mengaktifkan origin

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ app.use('*', cors({
     origin: process.env.NODE_ENV === 'development' ? ['http://localhost:3000'] : ['https://absensiswa.netlify.app'],
     methods: 'GET,PUT,POST,DELETE',
 }))
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//     next()
-// })
+
 app.use(bodyParser.json())
 
 app.use('/akun', akun)
